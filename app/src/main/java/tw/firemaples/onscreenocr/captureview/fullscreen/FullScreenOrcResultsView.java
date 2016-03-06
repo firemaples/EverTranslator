4776d1e8b6a8d8b9c850c128999e14f662f15ee8
firemaples <firemaples@gmail.com>
2016-03-07 02:41:07 +0800

    Text auto wrap when overflow

--- app/src/main/java/tw/firemaples/onscreenocr/captureview/fullscreen/FullScreenOrcResultsView.java
+++ app/src/main/java/tw/firemaples/onscreenocr/captureview/fullscreen/FullScreenOrcResultsView.java
@@ -5,8 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import java.util.List;
@@ -25,6 +30,7 @@ public class FullScreenOrcResultsView extends ImageView {
     private TextPaint textPaint;
 
     private int textBackgroundPadding = 5;
+    private DisplayMetrics metrics;
 
     public FullScreenOrcResultsView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -43,6 +49,11 @@ public class FullScreenOrcResultsView extends ImageView {
         textPaint.setColor(Color.BLUE);
         textPaint.setAntiAlias(true);
         textPaint.setTextSize(96);
+
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
     }
 
     public void setOrcResults(List<OrcResult> orcResults) {
@@ -59,11 +70,19 @@ public class FullScreenOrcResultsView extends ImageView {
                     Rect subRect = orcResult.getSubRect();
 
                     int textWidth = Math.max(subRect.width(), (int) textPaint.measureText(orcResult.getTranslatedText()));
+                    textWidth = Math.min(textWidth, metrics.widthPixels - subRect.left - 16);
                     int textHeight = Math.max(subRect.height(), (int) textPaint.getTextSize());
 
                     canvas.drawRect(new Rect(subRect.left, subRect.top, subRect.left + textWidth, subRect.top + textHeight), boxPaint);
 
-                    canvas.drawText(orcResult.getTranslatedText(), subRect.left, subRect.bottom, textPaint);
+                    StaticLayout sl = new StaticLayout(orcResult.getTranslatedText(), textPaint, textWidth, Layout.Alignment.ALIGN_NORMAL, 1, 1, false);
+
+                    canvas.save();
+                    canvas.translate(subRect.left, subRect.top);
+                    sl.draw(canvas);
+                    canvas.restore();
+
+//                    canvas.drawText(orcResult.getTranslatedText(), subRect.left, subRect.bottom, textPaint);
                 }
             }
         }
