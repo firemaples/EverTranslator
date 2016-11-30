a3246851472c0d9f67bb7f2d4b823f6b146e2e4e
firemaples <firemaples@gmail.com>
2016-11-30 17:48:56 +0800

    New style

--- app/src/main/java/tw/firemaples/onscreenocr/views/OcrResultWindow.java
+++ app/src/main/java/tw/firemaples/onscreenocr/views/OcrResultWindow.java
@@ -1,8 +1,11 @@
 package tw.firemaples.onscreenocr.views;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -16,11 +19,16 @@ import tw.firemaples.onscreenocr.utils.ViewPreparedWaiter;
  */
 
 public class OcrResultWindow {
+    private final static int MARGIN = 10;
+
     private final Context context;
     private ViewGroup parent;
+    private View anchorView;
+
     private View rootView;
     private TextView tv_originText, tv_translatedText;
     private FrameLayout.LayoutParams layoutParams;
+    private DisplayMetrics metrics;
 
     public OcrResultWindow(Context context, ViewGroup parent) {
         this.context = context;
@@ -31,6 +39,12 @@ public class OcrResultWindow {
         tv_translatedText = (TextView) rootView.findViewById(R.id.tv_translatedText);
 
         layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
     }
 
     public void setOcrResult(OcrResult ocrResult) {
@@ -40,6 +54,7 @@ public class OcrResultWindow {
 
     public void show(View anchorView) {
         dismiss();
+        this.anchorView = anchorView;
 
         new ViewPreparedWaiter().waitView(rootView, onViewPrepared);
         parent.addView(rootView, layoutParams);
@@ -51,10 +66,34 @@ public class OcrResultWindow {
         }
     }
 
+    private void adjustViewPosition() {
+        int width = rootView.getWidth();
+        int height = rootView.getHeight();
+
+        if (anchorView.getTop() > height + MARGIN * 2) {
+            //Gravity = TOP
+            layoutParams.topMargin = anchorView.getTop() - height;
+        } else {
+            //Gravity = BOTTOM
+            layoutParams.topMargin = anchorView.getTop() + anchorView.getHeight();
+        }
+
+        if (anchorView.getLeft() + width + MARGIN > metrics.widthPixels) {
+            // Match screen right
+            layoutParams.leftMargin = metrics.widthPixels - (width + MARGIN);
+        } else {
+            // Match anchorView left
+            layoutParams.leftMargin = anchorView.getLeft();
+        }
+
+        parent.updateViewLayout(rootView, layoutParams);
+    }
+
     private ViewPreparedWaiter.OnViewPrepared onViewPrepared = new ViewPreparedWaiter.OnViewPrepared() {
         @Override
         public void onViewPrepared(View viewToWait) {
             Tool.logInfo("Width:" + viewToWait.getWidth() + " Height:" + viewToWait.getHeight());
+            adjustViewPosition();
         }
     };
 }
