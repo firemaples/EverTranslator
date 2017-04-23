745bfffa8f8c47e75464bcef90586ac6003d96b2
firemaples <firemaples@gmail.com>
2017-04-23 14:07:01 +0800

    Add KeyId file into git

---
+++ app/src/main/java/tw/firemaples/onscreenocr/utils/KeyId.java
@@ -0,0 +1,8 @@
+package tw.firemaples.onscreenocr.utils;
+
+/**
+ * Created by firemaples on 2016/3/7.
+ */
+public class KeyId {
+    public final static String MICROSOFT_TRANSLATE_SUBSCRIPTION_KEY = "d0fdbac3cc9c404bb643faf98af01458";
+}
