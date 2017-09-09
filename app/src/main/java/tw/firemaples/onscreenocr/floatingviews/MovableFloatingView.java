93275a7bcd7f39711c2b4592453b700a427bcf1a
firemaples <firemaples@gmail.com>
2017-09-09 21:08:09 +0800

    Make main window starting from right top

---
+++ app/src/main/java/tw/firemaples/onscreenocr/floatingviews/MovableFloatingView.java
@@ -0,0 +1,79 @@
+package tw.firemaples.onscreenocr.floatingviews;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by louis1chen on 01/09/2017.
+ */
+
+public abstract class MovableFloatingView extends FloatingView {
+
+    public MovableFloatingView(Context context) {
+        super(context);
+    }
+
+    protected void setDragView(View view) {
+        view.setOnTouchListener(onTouchListener);
+    }
+
+    private boolean isAlignParentLeft() {
+        return (Gravity.getAbsoluteGravity(getLayoutGravity(), getRootView().getLayoutDirection()) & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.LEFT;
+    }
+
+    private boolean isAlignParentTop() {
+        return (getLayoutGravity() & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.TOP;
+    }
+
+    private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+        private int initX, initY;
+        private float initTouchX, initTouchY;
+        private boolean hasMoved = false;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    hasMoved = false;
+                    initX = getFloatingLayoutParams().x;
+                    initY = getFloatingLayoutParams().y;
+                    initTouchX = event.getRawX();
+                    initTouchY = event.getRawY();
+//                    Tool.logInfo("Action down: initX" + initX + " initY:" + initY + " initTouchX:" + initTouchX + " initTouchY:" + initTouchY);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (hasMoved) {
+                        return true;
+                    }
+                    hasMoved = false;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (Math.abs(initTouchX - event.getRawX()) > 20 || Math.abs(initTouchY - event.getRawY()) > 20) {
+                        hasMoved = true;
+                    }
+                    int xDiff = (int) (event.getRawX() - initTouchX);
+                    int yDiff = (int) (event.getRawY() - initTouchY);
+
+                    //TODO Not working when align parent center
+                    int nextX = initX + (isAlignParentLeft() ? xDiff : -xDiff);
+                    int nextY = initY + (isAlignParentTop() ? yDiff : -yDiff);
+
+                    if (nextX < 0) {
+                        nextX = 0;
+                    }
+                    if (nextY < 0) {
+                        nextY = 0;
+                    }
+                    getFloatingLayoutParams().x = nextX;
+                    getFloatingLayoutParams().y = nextY;
+                    getWindowManager().updateViewLayout(getRootView(), getFloatingLayoutParams());
+//                    Tool.logInfo("Touch location: x:" + event.getRawX() + " y:" + event.getRawY());
+//                    Tool.logInfo("New location: x:" + nextX + " y:" + nextY);
+                    break;
+            }
+            return false;
+        }
+    };
+}
--- app/src/main/java/tw/firemaples/onscreenocr/floatingviews/screencrop/FloatingBar.java
+++ app/src/main/java/tw/firemaples/onscreenocr/floatingviews/screencrop/FloatingBar.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.os.AsyncTask;
-import android.view.MotionEvent;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -16,7 +16,7 @@ import java.util.Locale;
 import tw.firemaples.onscreenocr.MainActivity;
 import tw.firemaples.onscreenocr.R;
 import tw.firemaples.onscreenocr.ScreenTranslatorService;
-import tw.firemaples.onscreenocr.floatingviews.FloatingView;
+import tw.firemaples.onscreenocr.floatingviews.MovableFloatingView;
 import tw.firemaples.onscreenocr.ocr.OcrDownloadAsyncTask;
 import tw.firemaples.onscreenocr.screenshot.ScreenshotHandler;
 import tw.firemaples.onscreenocr.utils.AppMode;
@@ -31,7 +31,7 @@ import tw.firemaples.onscreenocr.views.FloatingBarMenu;
  * Created by firemaples on 21/10/2016.
  */
 
-public class FloatingBar extends FloatingView {
+public class FloatingBar extends MovableFloatingView {
     private View view_menu, bt_selectArea, bt_translation, bt_clear;
     private Spinner sp_langFrom, sp_langTo;
 
@@ -91,6 +91,11 @@ public class FloatingBar extends FloatingView {
         return R.layout.view_floating_bar;
     }
 
+    @Override
+    protected int getLayoutGravity() {
+        return Gravity.TOP | Gravity.RIGHT;
+    }
+
     private void setViews(View rootView) {
         view_menu = rootView.findViewById(R.id.view_menu);
         bt_selectArea = rootView.findViewById(R.id.bt_selectArea);
@@ -99,7 +104,6 @@ public class FloatingBar extends FloatingView {
         sp_langFrom = (Spinner) rootView.findViewById(R.id.sp_langFrom);
         sp_langTo = (Spinner) rootView.findViewById(R.id.sp_langTo);
 
-        view_menu.setOnTouchListener(onTouchListener);
         view_menu.setOnClickListener(onClickListener);
         bt_selectArea.setOnClickListener(onClickListener);
         bt_translation.setOnClickListener(onClickListener);
@@ -117,6 +121,8 @@ public class FloatingBar extends FloatingView {
         sp_langTo.setOnItemSelectedListener(onItemSelectedListener);
 
         dialogView = new DialogView(getContext());
+
+        setDragView(view_menu);
 
         if (SharePreferenceUtil.getInstance().startingWithSelectionMode()) {
             onClickListener.onClick(bt_selectArea);
@@ -210,51 +216,6 @@ public class FloatingBar extends FloatingView {
         public void onProgressViewAttachedToWindow() {
             FloatingBar.this.detachFromWindow(false);
             FloatingBar.this.attachToWindow();
-        }
-    };
-
-    private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
-        private int initX, initY;
-        private float initTouchX, initTouchY;
-        private boolean hasMoved = false;
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    hasMoved = false;
-                    initX = getFloatingLayoutParams().x;
-                    initY = getFloatingLayoutParams().y;
-                    initTouchX = event.getRawX();
-                    initTouchY = event.getRawY();
-//                    Tool.logInfo("Action down: initX" + initX + " initY:" + initY + " initTouchX:" + initTouchX + " initTouchY:" + initTouchY);
-                    break;
-                case MotionEvent.ACTION_UP:
-                    if (hasMoved) {
-                        return true;
-                    }
-                    hasMoved = false;
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    if (Math.abs(initTouchX - event.getRawX()) > 20 || Math.abs(initTouchY - event.getRawY()) > 20) {
-                        hasMoved = true;
-                    }
-                    int nextX = initX + (int) (event.getRawX() - initTouchX);
-                    int nextY = initY + (int) (event.getRawY() - initTouchY);
-                    if (nextX < 0) {
-                        nextX = 0;
-                    }
-                    if (nextY < 0) {
-                        nextY = 0;
-                    }
-                    getFloatingLayoutParams().x = nextX;
-                    getFloatingLayoutParams().y = nextY;
-                    getWindowManager().updateViewLayout(getRootView(), getFloatingLayoutParams());
-//                    Tool.logInfo("Touch location: x:" + event.getRawX() + " y:" + event.getRawY());
-//                    Tool.logInfo("New location: x:" + nextX + " y:" + nextY);
-                    break;
-            }
-            return false;
         }
     };
 
