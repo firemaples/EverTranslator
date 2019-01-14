1da9f50034ca73b92f32a52444dcede3bfc28462
firemaples <firemaples@gmail.com>
2019-01-14 16:53:12 +0800

    Enhance event for translation error & send non-fatal exception

--- app/src/main/java/tw/firemaples/onscreenocr/state/TranslatingState.kt
+++ app/src/main/java/tw/firemaples/onscreenocr/state/TranslatingState.kt
@@ -4,9 +4,11 @@ import tw.firemaples.onscreenocr.StateManager
 import tw.firemaples.onscreenocr.StateName
 import tw.firemaples.onscreenocr.log.FirebaseEvent
 import tw.firemaples.onscreenocr.translate.TranslationManager
+import tw.firemaples.onscreenocr.translate.TranslationService
 import tw.firemaples.onscreenocr.translate.TranslationUtil
 
 object TranslatingState : OverlayState() {
+    private var service: TranslationService? = null
 
     override fun stateName(): StateName = StateName.Translating
 
@@ -15,6 +17,8 @@ object TranslatingState : OverlayState() {
         if (manager.ocrResultList.isEmpty() || manager.ocrResultText.isNullOrBlank()) {
             onTranslated(manager, "")
         } else {
+            service = TranslationUtil.currentService
+
             TranslationManager.startTranslation(manager.ocrResultText ?: "",
                     TranslationUtil.currentTranslationLangCode) { isSuccess, result, throwable ->
                 if (isSuccess) {
@@ -27,14 +31,15 @@ object TranslatingState : OverlayState() {
     }
 
     private fun onTranslated(manager: StateManager, text: String) {
-        FirebaseEvent.logTranslationTextFinished()
+        FirebaseEvent.logTranslationTextFinished(service)
         manager.translatedText = text
         manager.dispatchOnTranslated()
         manager.enterState(TranslatedState)
     }
 
     private fun onTranslationFailed(manager: StateManager, t: Throwable?) {
-        FirebaseEvent.logTranslationTextFailed()
+        FirebaseEvent.logTranslationTextFailed(service)
+        t?.also { FirebaseEvent.logException(t) }
         manager.translatedText = ""
         manager.dispatchTranslationFailed(t)
         manager.enterState(TranslatedState)
