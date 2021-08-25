61256b8eb95d8b0b17539b1bfd63a0dddd153faa
firemaples <firemaples@gmail.com>
2021-08-25 14:13:13 +0800

    Update MainBar to support night mode

--- main/src/main/java/tw/firemaples/onscreenocr/floatings/main/MainBar.kt
+++ main/src/main/java/tw/firemaples/onscreenocr/floatings/main/MainBar.kt
@@ -2,6 +2,7 @@ package tw.firemaples.onscreenocr.floatings.main
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 import tw.firemaples.onscreenocr.R
 import tw.firemaples.onscreenocr.floatings.base.MovableFloatingView
 
@@ -9,15 +10,32 @@ class MainBar(context: Context) : MovableFloatingView(context) {
     override val layoutId: Int
         get() = R.layout.floating_main_bar
 
-    private val viewTest = rootView.findViewById<View>(R.id.view_test)
-
     override val moveToEdgeAfterMoved: Boolean
         get() = true
 
     override val fadeOutAfterMoved: Boolean
         get() = true
 
+    private val btLangSelector: View = rootView.findViewById(R.id.bt_langSelector)
+    private val tvLang: TextView = rootView.findViewById(R.id.tv_lang)
+    private val ivGoogleTranslator: View = rootView.findViewById(R.id.iv_googleTranslator)
+    private val btSelect: View = rootView.findViewById(R.id.bt_select)
+
+    //    private val btOCROnly: View = rootView.findViewById(R.id.bt_ocrOnly)
+    private val btClose: View = rootView.findViewById(R.id.bt_close)
+    private val btMenu: View = rootView.findViewById(R.id.bt_menu)
+
     init {
-        setDragView(viewTest)
+        setViews()
+        setDragView(btMenu)
+    }
+
+    private fun setViews() {
+        tvLang.text = "En>"
+        ivGoogleTranslator.visibility = View.VISIBLE
+
+        btSelect.visibility = View.VISIBLE
+//        btOCROnly.visibility = View.VISIBLE
+        btClose.visibility = View.VISIBLE
     }
 }
