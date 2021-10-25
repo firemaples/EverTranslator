cef49dbf0a76b27c5751ca6d02fee4a3fd57ef0a
firemaples <firemaples@gmail.com>
2021-10-25 13:21:46 +0800

    Add advertisment from AdMob

--- main/src/main/java/tw/firemaples/onscreenocr/pages/launch/LaunchActivity.kt
+++ main/src/main/java/tw/firemaples/onscreenocr/pages/launch/LaunchActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import tw.firemaples.onscreenocr.R
 import tw.firemaples.onscreenocr.remoteconfig.RemoteConfigManager
+import tw.firemaples.onscreenocr.utils.MoPubAdManager
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        MoPubAdManager.loadPermissionPageBanner(this, findViewById(R.id.ad_permissionPage))
 
         RemoteConfigManager.tryFetchNew()
     }
