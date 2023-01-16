f4ffb0a17f7701b92a2d74544b14bbf2a9bc478a
uomar8 <omarharb444@yahoo.com>
2023-01-16 15:48:31 +0300

    Move requestBattery to settings page (#232)
    * Update permission_float_window_fragment.xml
    
    hide requestBattery at the beginning  so it won't show two buttons
    
    * Move bypass battery optimization option to the settings page
    
    Co-authored-by: firemaples <firemaples@gmail.com>

--- main/src/main/java/tw/firemaples/onscreenocr/pages/launch/permissions/PermissionCaptureScreenFragment.kt
+++ main/src/main/java/tw/firemaples/onscreenocr/pages/launch/permissions/PermissionCaptureScreenFragment.kt
@@ -2,21 +2,16 @@ package tw.firemaples.onscreenocr.pages.launch.permissions
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.media.projection.MediaProjectionManager
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import tw.firemaples.onscreenocr.databinding.PermissionCaptureScreenFragmentBinding
 import tw.firemaples.onscreenocr.floatings.ViewHolderService
 import tw.firemaples.onscreenocr.screenshot.ScreenExtractor
-import tw.firemaples.onscreenocr.utils.Utils
 import tw.firemaples.onscreenocr.utils.clickOnce
 
 class PermissionCaptureScreenFragment : Fragment() {
@@ -47,17 +42,6 @@ class PermissionCaptureScreenFragment : Fragment() {
         binding.btRequestPermission.clickOnce {
             requestMediaProject()
         }
-
-        binding.btRequestBatteryOpt.clickOnce {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                startActivity(Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS))
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.batteryOptimizationBlock.isVisible = Utils.batteryOptimized(requireContext())
     }
 
     private fun requestMediaProject() {
