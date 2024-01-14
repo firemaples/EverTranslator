1d6480d2f440677e966d5fa421572bf452e7c354
firemaples <firemaples@gmail.com>
2024-01-14 20:00:27 +0900

    Skip word boundary with blank + dash pattern

--- main/src/main/java/tw/firemaples/onscreenocr/utils/WordBoundary.kt
+++ main/src/main/java/tw/firemaples/onscreenocr/utils/WordBoundary.kt
@@ -1,7 +1,7 @@
 package tw.firemaples.onscreenocr.utils
 
 import java.text.BreakIterator
-import java.util.*
+import java.util.Locale
 
 object WordBoundary {
     fun breakWords(text: String, locale: Locale): List<Boundary> {
@@ -9,6 +9,7 @@ object WordBoundary {
         val boundaries = mutableListOf<Boundary>()
         var start = iterator.first()
         var end = iterator.next()
+        var previousWord: String? = null
         while (end != BreakIterator.DONE) {
             val word = text.substring(start, end)
             if (word.isPunctuation() || word.isBlank()) {
@@ -24,12 +25,17 @@ object WordBoundary {
                     if (boundaries.isNotEmpty() && boundaries.last().word.isDash()) {
                         boundaries.removeLast()
                     }
-                    boundaries.add(Boundary(word, start, end))
+                    if (word.isDash() && previousWord?.isBlank() == true) {
+                        // Skip dash following blank pattern. " -"
+                    } else {
+                        boundaries.add(Boundary(word, start, end))
+                    }
                 }
             }
 
             start = end
             end = iterator.next()
+            previousWord = word
         }
 
         return boundaries
