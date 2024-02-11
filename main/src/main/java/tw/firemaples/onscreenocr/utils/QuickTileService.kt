d5fe0c0b2b07ab40f57060aad6cf8028e30ddd48
firemaples <firemaples@gmail.com>
2024-02-11 23:13:02 +0900

    Upgrade target version to 34

--- main/src/main/java/tw/firemaples/onscreenocr/utils/QuickTileService.kt
+++ main/src/main/java/tw/firemaples/onscreenocr/utils/QuickTileService.kt
@@ -7,7 +7,11 @@ import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import tw.firemaples.onscreenocr.floatings.manager.FloatingViewCoordinator
 import tw.firemaples.onscreenocr.pages.launch.LaunchActivity
 import tw.firemaples.onscreenocr.screenshot.ScreenExtractor
@@ -52,7 +56,11 @@ class QuickTileService : TileService() {
             if (ScreenExtractor.isGranted) {
                 floatingViewCoordinator.showMainBar()
             } else {
-                startActivityAndCollapse(LaunchActivity.getLaunchIntent(this))
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    startActivityAndCollapse(LaunchActivity.getLaunchPendingIntent(this))
+                } else {
+                    startActivityAndCollapse(LaunchActivity.getLaunchIntent(this))
+                }
             }
         }
     }
